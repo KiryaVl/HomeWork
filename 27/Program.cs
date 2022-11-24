f11363173af5c8dc783d4b4a5ab4cc4e12144a17
Kirya <rabbitnya@gmail.com>
2022-11-24 14:55:36 +0300

    Complete homework 27

---
+++ 27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int x = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int sum=0;
+for (int i=3; i>=0; i--)
+{
+    sum+=x/(int)Math.Pow(10.0,i);
+    x=x%(int)Math.Pow(10.0,i);
+}
+Console.WriteLine($"Сумма цифр в числе = {sum}");
