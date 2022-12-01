f7c59cc5600dca51c27e351c31e3fb5db2a5905d
Kirya <rabbitnya@gmail.com>
2022-12-01 20:05:26 +0300

    Complete homework 41

---
+++ 41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите числа: ");
+int[] input = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);   //Array.ConvertAll - Преобразует массив одного типа в массив другого типа
+int sum = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine(sum);
